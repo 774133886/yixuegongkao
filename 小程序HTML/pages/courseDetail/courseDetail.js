8dea8448b2ad3b5098b173747237b2149328fcef
Chris-Clear <602294138@qq.com>
2019-08-03 09:50:17 +0800

    83950

---
+++ 小程序HTML/pages/courseDetail/courseDetail.js
@@ -0,0 +1,75 @@
+// pages/courseDetail/courseDetail.js
+const util = require('../../utils/util.js')
+const http = require('../../http.js')
+const app = getApp();
+var WxParse = require('../../wxParse/wxParse.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    shareShow:false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    var content = "<img style='margin-left: 0;border-radius:10px; width:100%;' src='../../files/indexBanner.png'/><div>此处为富文本<div/>" ;
+    WxParse.wxParse('article', 'html', content, that, 5);
+  },
+  //分享遮罩
+  shareTab: function (e) {
+    this.setData({ shareShow: !this.data.shareShow })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
