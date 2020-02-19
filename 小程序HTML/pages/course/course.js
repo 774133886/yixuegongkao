b6f43eb96a4f21e0cbaa9e89b07da43fe3f503ad
yvan <774133886@qq.com>
2020-02-19 18:23:30 +0800

    5614

--- 小程序HTML/pages/course/course.js
+++ 小程序HTML/pages/course/course.js
@@ -11,7 +11,9 @@ Page({
   data: {
     active: 0,
     list: ["6","1","2","3","4","8","9"],
-    hd_list: [],
+    hd_list: [
+      [], [], [], [], [], [], []
+    ],
     courseList: [
       [],[],[],[],[],[],[]
     ],
@@ -79,38 +81,48 @@ Page({
     var params = Object.assign(data, obj ? obj : {})
     http.postReq('/api/public/get_course_list.htm', params,function(res){
       if (res.code == 0) {
-        if (data.promotionType = 'all') {
+        // if (data.promotionType = 'all') {
           res.data.list.forEach(function (v, i) {
             v.last_time = (new Date(v.apply_end_time.replace('-', '/').replace('-', '/')).getTime() - Date.parse(new Date())) / 1000;
           });
-          that.startTime()
-        }
+        // }
         var list = that.data.courseList;
         var pages = that.data.pageList;
         pages[that.data.active] = res.data.pagination;
         if (list[that.data.active].length == 0 || that.data.isShow){
           list[that.data.active] = res.data.list;
-          if (data.promotionType = 'all') {
-            that.setData({
-              hd_list: list[0]
-            })
-          }
+          // if (data.promotionType = 'all') {
+            // that.setData({
+            //   hd_list: list[0]
+            // })
+          // }
         }else{
           list[that.data.active] = list[that.data.active].concat(res.data.list);
-          if (data.promotionType = 'all') {
-            that.setData({
-              hd_list: list[0]
-            })
-          }
+          // if (data.promotionType = 'all') {
+            // that.setData({
+            //   hd_list: list[0]
+            // })
+          // }
         }
         var scrollList = that.data.scrollList;
+        var hd_list = that.data.hd_list;
+        hd_list[that.data.active] = [];
+        list[that.data.active].forEach(function (item, index) {
+          if (item.last_time>0){
+            hd_list[that.data.active].push({ last_time: item.last_time})
+          }else{
+            hd_list[that.data.active].push({})
+          }
+        })
         that.setData({
           courseList: list,
           pageList: pages,
           isShow: false,
           refreshing: false,
-          scrollTop: scrollList
+          scrollTop: scrollList,
+          hd_list: hd_list
         })
+        that.startTime()
       }
     })
   },
@@ -141,31 +153,35 @@ Page({
   },
   startTime() {
     var that = this;
-    var aclist = [];
+    // var aclist = [];
     clearInterval(pt_set);
     pt_set = setInterval(time1, 1000);
     function time1() {
-      aclist = that.data.hd_list;
+      var hd_list = that.data.hd_list;
+      hd_list.forEach(function (aclist,index){
+        aclist.forEach(function (a, i) {
+          var time = a.last_time
+          if (a.last_time) {
+            if (a.last_time == 1) {
+              a.last_time = 0;
+              clearInterval(countTime);
+              // 重新获取数据
+              that.getList();
+            } else {
+              a.last_time--;
+            }
+            that.setData({
+              hd_list: hd_list
+            })
 
-      aclist.forEach(function (a, i) {
-        var time = a.last_time
-        if (a.last_time) {
-          if (a.last_time == 1) {
-            a.last_time = 0;
-            clearInterval(countTime);
-            // 重新获取数据
-            that.getList();
           } else {
-            a.last_time--;
+            return false;
           }
-          that.setData({
-            hd_list: aclist
-          })
-
-        } else {
-          return false;
-        }
+        })
       })
+      // aclist = that.data.hd_list;
+
+      
     }
   },
   //回到顶部
